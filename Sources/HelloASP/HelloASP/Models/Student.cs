37ea7ea592c7cf9c797dd44f53aee3a92391510c
iamthankyou <quangduynb00@gmail.com>
2021-03-08 01:05:21 +0700

    Data anotation

--- Sources/HelloASP/HelloASP/Models/Student.cs
+++ Sources/HelloASP/HelloASP/Models/Student.cs
@@ -16,15 +16,20 @@ namespace HelloASP.Models
         }
 
         [StringLength(20)]
+        [Display(Name = "Student ID")]
         public string StudentId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter firstname")]
         [StringLength(50)]
+        [Display(Name = "First Name")]
         public string FisrtName { get; set; }
 
+        [Display(Name = "Last Name")]
         [StringLength(50)]
         public string LastName { get; set; }
 
+        //[RegularExpression(@"^(((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])(\/)\d{4}$", ErrorMessage = "Not type datetime")]
+        [DataType(DataType.DateTime, ErrorMessage = "Please enter a valid date in the format")]
         public DateTime? Grade { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
